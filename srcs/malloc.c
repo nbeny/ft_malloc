5ff37d447eb1054f52175eec85df8a85678641cd
Nicolas BENY <nbeny@e2r7p15.42.fr>
2018-03-21 11:07:27 +0100

    malloc

--- srcs/malloc.c
+++ srcs/malloc.c
@@ -11,18 +11,21 @@ void	*ft_malloc(size_t size)
 		return (NULL);
 	else if (size < 64)
 	{
+		ft_printf(2, "tiny\n");
 		if (!check_page_tiny(g_pg, size))
 			ptr = add_page(g_pg, size, 0);
 		ptr = get_ptr_tiny(ptr);
 	}
 	else if (size >= 64 && size < 1024)
 	{
+		ft_printf(2, "small\n");
 		if (!check_page_small(g_pg, size))
 			ptr = add_page(g_pg, size, 1);
 		ptr = get_ptr_small(ptr);
 	}
 	else
 	{
+		ft_printf(2, "large\n");
 		ptr = add_page(g_pg, size, 2);
 	}
 	if (ptr == NULL)
